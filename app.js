5d43cc4ce7c76e783d7644232d63e543c6c333ea
kyle uyehara <kuyehara1@gmail.com>
2016-10-14 20:55:35 -1000

    test more jQuery

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-$(document).ready(function() {
+function eventHandlerHookup() {
 
-   var that;
-
-   $("#quoteStation").click(function(event) {
-      $("#quoteStation").css("background-color", "purple");
-      alert(event.currentTarget === that);
-      console.log(event.currentTarget)
-   })
-
-
-   $("#quoteBtn").click(function(event) {
-      var currBg = $(".stationRow").css("background-color");
-      var currMarTop = $(".stationRow").css("margin-top");
+   function quoteBtnClick(eventObject) {
+     var currBg = stationRow.css("background-color");
+      var currMarTop = stationRow.css("margin-top");
       var currMarTopNum = Number(currMarTop.slice(0, currMarTop.length - 2));
-      that = this;
 
       if(currBg != "rgb(0, 0, 255)") {
          currBg = "rgb(0, 0, 255)";
       } else {
          currBg = "rgb(0, 255, 0)";
       }
+
+      stationRow
+         .css("background-color", currBg)
+         .css("margin-top", ++currMarTopNum);
+   }
+
+   var quoteStation = $("#quoteStation");
+   var stationRow = $(".stationRow");
+   var quoteBtn = $("#quoteBtn");
+
+   quoteStation.click( function(eventObject) {
+      quoteStation.css("background-color", "purple");
+   })
+
+   quoteBtn.on("click", { foo: "bar" }, quoteBtnClick );
+}
+
+$(document).ready(eventHandlerHookup);
 
       // var xml = new XMLHttpRequest();
       // xml
@@ -32,12 +40,3 @@ $(document).ready(function() {
       // var p = $("p")
       // p.html("Lucky me");
       // $(".stationRow").append(p);
-      $(".stationRow")
-         .css("background-color", currBg)
-         .css("margin-top", ++currMarTopNum);
-
-   })
-   $("#quoteBtn")
-      .click();
-
-});
