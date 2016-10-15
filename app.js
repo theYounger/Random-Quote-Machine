dab162945cdc1f48d25bacf03ad00feec1ba7941
kyle uyehara <kuyehara1@gmail.com>
2016-10-14 19:00:42 -1000

    test out jquery concepts like custom events and triggers

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-// $(document).ready(function() {
-   $("#quoteBtn").click(function() {
+$(document).ready(function() {
+
+   var that;
+
+   $("#quoteStation").click(function(event) {
+      $("#quoteStation").css("background-color", "purple");
+      alert(event.currentTarget === that);
+      console.log(event.currentTarget)
+   })
+
+
+   $("#quoteBtn").click(function(event) {
       var currBg = $(".stationRow").css("background-color");
+      var currMarTop = $(".stationRow").css("margin-top");
+      var currMarTopNum = Number(currMarTop.slice(0, currMarTop.length - 2));
+      that = this;
 
       if(currBg != "rgb(0, 0, 255)") {
          currBg = "rgb(0, 0, 255)";
       } else {
          currBg = "rgb(0, 255, 0)";
       }
+
       // var xml = new XMLHttpRequest();
       // xml
       //    .onload = function(json) {
@@ -18,7 +32,12 @@
       // var p = $("p")
       // p.html("Lucky me");
       // $(".stationRow").append(p);
-      $(".stationRow").css("background-color", currBg);
+      $(".stationRow")
+         .css("background-color", currBg)
+         .css("margin-top", ++currMarTopNum);
 
-   });
-// });
+   })
+   $("#quoteBtn")
+      .click();
+
+});
